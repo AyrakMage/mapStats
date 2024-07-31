99146e7685fa2f2af152bce9f8874dad68f554b1
Pallav Karya <pallavkarya@Pallavs-MacBook-Pro.local>
2024-07-31 11:48:06 +0530

    Setup screens

---
+++ screens/homePage/homepage.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+const Homepage = (props) => {
+  return (
+    <View>
+      <Text>Hello Map View</Text>
+    </View>
+  )
+}
+
+export default Homepage
---
+++ screens/stateDetailPage/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StateDetailPage = (props) => {
+  return (
+    <View>
+      <Text>StateDetailPage</Text>
+    </View>
+  )
+}
+
+export default StateDetailPage
